c1c4f487b6df691eca4d5c8fcba9d10c8e3ad492
Denis Sergeev <denis-sergeev-85@inbox.ru>
2020-09-25 23:03:35 +0300

    analytics - getMonth

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -4,35 +4,43 @@ const mainAnswer = document.querySelector('.question__headline-main-answer');
 const newsAnswerweek = document.querySelector('.question__analysis-news-answerweek');
 const ans = document.querySelector('.question__analysis-news-answerweek_1');
 const mainAsk = document.querySelector('.question__headline-main-ask');
-const columnDays = document.querySelector('.analytics__column-days');
+const columnDays = document.querySelector('.analytics__column-days');  //даты
 const columnHistogram = document.querySelector('.analytics__column-histogram');
-const month1 = document.querySelector('.analytics__diagram-data');   //август
+const month = document.querySelector('.analytics__diagram-data');   //месяц
 const data = JSON.parse(localStorage.getItem(CARDS)); //JSON-массив
+console.log(data)
+const answer = localStorage.getItem('input'); 
+
+
 
 mainAnswer.textContent = localStorage.getItem('input');  //вы спросили
-newsAnswerweek.textContent = localStorage.getItem('input'); //новости за неделю
-ans.textContent = data.totalResults;   //заголовок
+//ans.textContent = localStorage.getItem('input'); //новости за неделю
+newsAnswerweek.textContent = data.totalResults;   //заголовок
+/////////////////////////////////////////////////////////////////////////////////////
+ans.textContent = references(answer, data.articles);  //упонимание за неделю или data или answer
 
-
-//month1.textContent = new Date();
-//month1.textContent = dat
-/*
-const dat = new Date();
-Month = console.log(dat.getMonth()+1);
-
-switch (Month)
-{
-  case 0: Month="января"; break;
-  case 1: Month="февраля"; break;
-  case 2: Month="марта"; break;
-  case 3: Month="апреля"; break;
-  case 4: Month="мае"; break;
-  case 5: Month="июня"; break;
-  case 6: Month="июля"; break;
-  case 7: Month="августа"; break;
-  case 8: Month="сентябрь"; break;
-  case 9: Month="октября"; break;
-  case 10: Month="ноября"; break;
-  case 11: Month="декабря"; break;
+function references(key, articles) { ////упонимание за неделю
+  let count = 0;
+  articles.forEach((i) => {
+    i.title = i.title.toLowerCase();
+    key = key.toLowerCase();
+    if(i.title.includes(key)) {
+      return count++;
+    }
+  })
+  return count;
 }
-*/
+////////////////////////////////////////////////////////////////////////
+///месяц
+const now = new Date();
+const mon = now.getMonth();
+const mons = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь']
+month.textContent = mons[mon]
+
+//Даты////////////////////////////////////
+
+const nows = new Date();
+const day = nows.getDate();
+const days = ['вс','пн','вт','ср','чт','пт','сб']
+columnDays.textContent = day;
+console.log(day)
